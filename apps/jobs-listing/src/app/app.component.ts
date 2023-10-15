b93d3097459409f385890a44d4035f166d227718
Egisa <egisa.v@idobet.com>
2023-10-15 11:22:33 +0200

    Initial commit

---
+++ apps/jobs-listing/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jobs-app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'jobs-listing';
+}
